0df62734c3ba2c7efd492d526cd8307015e02b9b
Jakub Olcha <olo75jg12@gmail.com>
2023-08-26 23:56:19 +0200

    fix

--- Flatie.Api/Controllers/ShoppingListController.cs
+++ Flatie.Api/Controllers/ShoppingListController.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Flatie.Api.Controllers.Interfaces;
+using Flatie.Dto.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Flatie.Api.Controllers
 {
-    public class ShoppingListController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShoppingListController : ControllerBase, IShoppingListController
     {
-
     }
 }
